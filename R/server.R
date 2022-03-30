c7286ed365adf80413e0935cd7ebe8294a0332a4
Frederic-gci <frederic.fournier@gci.ulaval.ca>
2022-03-30 11:47:01 -0400

    Skeletton with shiny, shinydashboard

---
+++ R/server.R
@@ -0,0 +1,7 @@
+#' The application server-side
+#'
+#' @param input,output,session Internal parameters for {shiny}.
+#'     DO NOT REMOVE.
+#' @import shiny
+#' @noRd
+server <- function(input, output, session) {}
---
+++ R/ui.R
@@ -0,0 +1,59 @@
+#' The application User-Interface
+#'
+#' @param request Internal parameter for `{shiny}`.
+#'     DO NOT REMOVE.
+#' @import shiny
+#' @import shinydashboard
+#' @noRd
+#'
+ui <- function(request) {
+  tagList(
+    shinydashboard::dashboardPage(
+      title = "shinyPOC",
+      dashboardHeader(title = "POC Shiny"),
+      dashboardSidebar(
+        sidebarMenu(
+          menuItem("Analyse",
+            tabName = "analysis",
+            icon = icon("chart-bar")
+          ),
+          menuItem("Utilitaires",
+            tabName = "utils",
+            icon = icon("toolbox")
+          ),
+          menuItem("Information",
+            tabName = "info",
+            icon = icon("info")
+          )
+        )
+      ),
+      dashboardBody(
+        tabItems(
+          tabItem(
+            tabName = "analysis",
+            h2("Analyse")
+          ),
+          tabItem(
+            tabName="utils",
+            box(
+              title = "Page des utilitaires",
+              status = "primary",
+              solidHeader = FALSE,
+              p("Cette page pourrait contenir des fonctions utilitaires permettant de construire adéquatement les données primaires."),
+            )
+          ),
+          tabItem(
+            tabName="info",
+            box(
+              title = "Page d'information",
+              status = "primary",
+              solidHeader = FALSE,
+              p("Cette page pourrait contenir l'information sur le projet."),
+            )
+
+          )
+        )
+      )
+    )
+  )
+}
